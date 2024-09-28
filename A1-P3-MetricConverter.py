f45672c4b344f84ab3a8d55ac26d13ca78f360d5
Dan Shaw <w0190983@nscc.ca>
2024-09-28 13:21:28 -0300

    Updated validateFloat

--- A1-P3-MetricConverter.py
+++ A1-P3-MetricConverter.py
@@ -1,17 +1,49 @@
-#Program 3 – Imperial to Metric Conversion
-#Write a console program that converts a weight given in tons (imperial), stones, pounds, and ounces 
-# to the metric equivalent in metric tons, kilograms, and grams.
+"""
+    Student Name: Dan Shaw w0190983
+    Program Name: Imperial to Metric Conversion
+    Program Description: A console program that converts a weight given in imperial tons, stones, pounds and ounces to the metric equivalent in metric tons, kilograms and grams
+"""
 
 def main():
-    # YOUR CODE STARTS HERE, each line must be indented (one tab)
+    # Output program title and greeting
+    greeting = "IMPERIAL TO METRIC CONVERTER"
+    print(greeting)
+    print("-" * len(greeting))
 
+    # Get the following values from the user: imperial tons, stone, pounds, ounces
+    while (imperialTons := validateFloat(input("\tEnter the number of tons: "))) == 0:
+        pass
 
+    while (stone := validateFloat(input("\tEnter the number of stone: "))) == 0:
+        pass
 
+    while (pounds := validateFloat(input("\tEnter the number of pounds: "))) == 0:
+        pass
 
+    while (ounces := validateFloat(input("\tEnter the number of ounces: "))) == 0:
+        pass
 
+    # Perform imperial to metric calculations
+    totalOunces = (35840 * imperialTons) + (224 * stone) + (16 * pounds) + ounces
+    totalKilos = totalOunces / 35.274
+    metricTons = int(totalKilos / 1000)
+    remainderKilos = totalKilos % 1000
+    remainderGrams = (remainderKilos - int(remainderKilos)) * 1000
 
+    # Output results to user
+    print("The metric weight is {0} metric tons, {1} kilos and {2:.1f} grams.".format(metricTons, int(remainderKilos), remainderGrams))
 
+# Validates whether inputQuery is a valid number by trying to convert to a float
+# Returns 1 if a valid float, 0 if not
+def validateFloat(inputQuery):
+    try:
+        return float(inputQuery)
+    except:
+        errorString = "Please enter a valid number"
+        print("-" * len(errorString))
+        print(errorString)
+        print("-" * len(errorString))
+        return 0
 
-    # YOUR CODE ENDS HERE
-
-main()
+if __name__ == "__main__":
+    main()
